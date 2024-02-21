d1792973adb957ad333513d70f30ec75425f4c6b
dddmogui13 <dmytropotorocha213@gmail.com>
2024-02-21 21:54:50 +0200

    dd

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,91 @@
-import contactsService from "../services/contactsServices.js";
+import express from "express";
 
-export const getAllContacts = (req, res) => {};
+import {
+  listContacts,
+  contactById,
+  addContact,
+  removeContact,
+  editContact,
+} from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
-export const getOneContact = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await listContacts();
 
-export const deleteContact = (req, res) => {};
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const getContactById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await contactById(id);
 
-export const updateContact = (req, res) => {};
+    if (!contact) {
+      throw HttpError(404);
+    }
+
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedContact = await removeContact(id);
+
+    if (!deletedContact) {
+      throw HttpError(404, "Not found");
+    }
+
+    res.status(200).json(deletedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    const newContact = await addContact(req.body);
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    if (!Object.keys(req.body).length > 0) {
+      throw HttpError(400, "Body must have at least one field");
+    }
+    const { error } = updateContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    const { id } = req.params;
+  
+    const updatedContact = await editContact(id, req.body);
+    if (!updatedContact) {
+      throw HttpError(404, "Not found");
+    }
+
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,52 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { nanoid } from 'nanoid';
+
+const contactsPath = path.join('db', 'contacts.json');
+
+export async function listContacts() {
+  const allContatcs = await fs.readFile(contactsPath);
+  return JSON.parse(allContatcs);
+}
+
+export async function contactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+}
+
+export async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+export async function addContact({ name, email, phone }) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+export async function editContact(contactId, body) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { ...contacts[index], ...body, id: contactId };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return contacts[index];
+  
+}
